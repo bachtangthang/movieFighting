fb6da0f1ebbe8b3130c1846b1b74dc5efc3d51a0
bachtangthang <thang5217113@gmail.com>
2021-09-07 01:42:56 +0700

    add function autofill searching

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const fetchData = async () => {
+const fetchData = async (searhString) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'da4f2d33',
-            s: 'avengers',
+            s: searhString,
         }
     })
     console.log(response.data);
 }
 
-fetchData();
+const input = document.querySelector('input');
+
+let timeoutID;
+
+const onInput = (event) => {
+    if(timeoutID){
+        clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => {
+    fetchData(event.target.value);
+    }, 1000)
+};
+input.addEventListener('input', onInput);
